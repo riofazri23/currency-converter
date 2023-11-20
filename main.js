637f0325dc9b503399f82da287337cd3e35091dd
Rio Fazri Septian <rfazri7@gmail.com>
2023-11-20 15:43:48 +0700

    memperbaiki currency converter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const dom = document.addEventListener("DOMContentLoaded", ()=>{
-    document.querySelector("#currency-converter").addEventListener("submit",(event)=>{
+    document.querySelector("#currency-converter").addEventListener("submit",async(event)=>{
         event.preventDefault();
+        console.log("async funtcion call first")
 
         const{target:{from, to, amount}} = event;
 
@@ -12,28 +13,45 @@ const dom = document.addEventListener("DOMContentLoaded", ()=>{
             headers, 
         }
 
-        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
-        .then(response=>response.json())
-        .then(data=>{
-            // {
-            //     "success": true,
-            //     "query": {
-            //         "from": "EUR",
-            //         "to": "IDR",
-            //         "amount": 5
-            //     },
-            //     "info": {
-            //         "timestamp": 1700190063,
-            //         "rate": 16811.320447
-            //     },
-            //     "date": "2023-11-17",
-            //     "result": 84056.602235
-            // }
-            let {info,date,query:{to}, result} = data;
-            document.querySelector(".result").textContent = `As per the exchange rate: ${info.rate} for ${date}=> converted value in ${to} is ${result}`;
-        })
-        .catch(error => console.log(error));
+        //fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+        // .then(response=>response.json())
+        // .then(data=>{
+        //     // {
+        //     //     "success": true,
+        //     //     "query": {
+        //     //         "from": "EUR",
+        //     //         "to": "IDR",
+        //     //         "amount": 5
+        //     //     },
+        //     //     "info": {
+        //     //         "timestamp": 1700190063,
+        //     //         "rate": 16811.320447
+        //     //     },
+        //     //     "date": "2023-11-17",
+        //     //     "result": 84056.602235
+        //     // }
+        //     let {info,date,query:{to}, result} = data;
+            // document.querySelector(".result").textContent = `As per the exchange rate: ${info.rate} for ${date}=> converted value in ${to} is ${result}`;
+        // })
+        // .catch(error => console.log(error));
 
+        try{
+            let response = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+            const data = await response.json();
+            // console.log(data);
+            let {info,date,query:{to: convertedTo}, result} = data;
+            document.querySelector(".result").textContent = `As per the exchange rate: ${info.rate} for ${date}=> converted value in ${to} is ${result}`;
+        }catch(error){
+            console.log(error)
+        }finally{
+            console.log("Finally block")
+        }
+
+    })
+
+
+    document.querySelector("#currency-converter").addEventListener("submit",async(event)=>{
+        console.log("a regular synchronous function");
     })
 
 })
